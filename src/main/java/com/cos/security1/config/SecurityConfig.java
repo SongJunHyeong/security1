d9a729f9b22b351e0c7f885daa7a1b2e48c3dffa
SongJunHyeong <thdwnsgud79@naver.com>
2024-03-25 22:07:31 +0900

    no message

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -23,15 +23,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/user/**").authenticated()
+                .antMatchers("/user/**").authenticated() // 인증만 되면 들어갈 수 있는 주소
                 .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 //.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/loginForm");
-                //.loginProcessingUrl("/loginProc");
-                //.defaultSuccessUrl("/");
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login") //login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줍니다. 컨트롤러에 login을 안만들어도된다.
+                .defaultSuccessUrl("/");
     }
 }
